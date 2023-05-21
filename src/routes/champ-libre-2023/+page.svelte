60b96e89c1845507ed5306b063dc6d0eaec60b91
Florian Motteau <florian.motteau@nereide.fr>
2023-05-21 18:34:39 +0200

    Ajout page programme

---
+++ src/routes/champ-libre-2023/+page.svelte
@@ -0,0 +1,63 @@
+
+<script language="ts">
+    import rimbaultTransparent from '$lib/images/rimbault-transparent.png';
+    import affichePdf from '$lib/images/affiche.pdf';
+    import afficheSansTexte from '$lib/images/affiche-sanstexte.png';
+    import logoSmall from '$lib/images/logo-small.png';
+</script>
+
+<div class=" container">
+    <h1 class="frankie text-center mb-14">Champ Libre 2023</h1>
+    <div class="img-header text-center flex justify-center mb-14">
+        <img src={rimbaultTransparent} alt="Portrait de Louis Rimbault"/>
+    </div>
+
+    <h2 class="frankie text-center mb-8 mt-14">Mardi 27 juin 2023</h2>
+    <h3 class="frankie text-4xl mb-4 mt-14 text-center">
+        <a href="https://cafeshistoriques.com/programme/la-rencontre-de-louis-rimbault-et-des-causeries-anarchistes" target="_blank" class="frankie">
+            A la rencontre de Louis Rimbault et des causeries anarchistes
+        </a>
+    </h3>
+    <p class="text-2xl mb-4">
+        Sous la halle de Luynes, à 19h, conférence d'<a href="https://fr.wikipedia.org/wiki/Anne_Steiner" target="_blank">Anne Steiner</a> :
+        "A la rencontre de Louis Rimbault et des causeries anarchistes"
+    </p>
+    <p class="text-2xl mb-4">
+        Anne Steiner est maître de conférence en sociologie à l'université Paris-Nanterre, spécialiste notamment du mouvement
+        individualiste anarchiste dont se réclamait Louis Rimbault.
+    </p>
+    <p class="text-2xl mb-4">
+        Cette conférence se tient dans le cadre des <a href="https://cafeshistoriques.com/" target="_blank">
+        Cafés Historiques</a>, avec le soutien de la région Centre Val-de-Loire</p>
+
+    <h2 class="frankie text-center mb-8 mt-14">Dimanche 2 juillet 2023</h2>
+    <h3 class="frankie text-4xl mb-4 mt-14 text-center">Balade sonore autour de Louis Rimbault</h3>
+    <p class="text-2xl mb-4">
+        Départ à 10h30 sous la Halle de Luynes pour un promenade d'environ 1h, parsemée de podcasts audio,
+        qui vous amène à découvrir l’histoire de Louis Rimbault et de son école végétarienne Terre Libérée.
+    </p>
+    <p class="text-2xl mb-4">
+        Ce parcours sonore proposé par l’association "L'autre terre libérée" vous racontera cette expérience
+        qui a eu lieu à Luynes entre 1923 et 1949, et qui a disparu des mémoires collectives.
+    </p>
+    <p class="text-2xl mb-4">
+        Pour découvrir cette histoire, nous vous inviterons à scanner des codes QR installés tout au long
+        du parcours pour écouter des petites pastilles audio tout en cheminant jusqu’au terrain de l'association.
+    </p>
+
+    <h3 class="frankie text-4xl mb-4 mt-14 text-center">Pique-nique partagé sur le terrain de l’association</h3>
+    <p class="text-2xl mb-4">
+        Chaque participant est invité à préparer une spécialité culinaire,
+        à la déposer sur la table pour partager le moment convivial du repas.
+        Une buvette sera également disponible pour se désaltérer, proposant des boissons avec et sans alcool.
+    </p>
+
+    <h3 class="frankie text-4xl mb-4 mt-14 text-center">Fresque du climat</h3>
+    <p class="text-2xl mb-4">
+        L’approche ludique et pédagogique de la Fresque du Climat permet à chacun de comprendre le fonctionnement,
+        l’ampleur et la complexité des enjeux liés aux dérèglements climatiques. L’atelier mobilise l’intelligence
+        collective pour sensibiliser les participants aux changements climatiques. Durant l’atelier, les participants
+        relient les liens de cause à effets et intègrent les enjeux climatiques dans leur globalité.
+    </p>
+</div>
+
