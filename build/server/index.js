6fa09c939669d200746f824ca32c8766e9967fb3
Florian Motteau <florian.motteau@nereide.fr>
2023-06-14 10:58:22 +0200

    Ajout ardoise 3

--- build/server/index.js
+++ build/server/index.js
@@ -182,7 +182,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1ges43i"
+  version_hash: "glk50e"
 };
 function get_hooks() {
   return {};
--- build/server/manifest.js
+++ build/server/manifest.js
@@ -4,10 +4,10 @@ const manifest = {
 	assets: new Set(["audio/1.mp3","audio/2.mp3","audio/3.mp3","audio/4.mp3","audio/5.mp3","audio/6.mp3","audio/7.mp3","audio/8.mp3","favicons/android-chrome-192x192.png","favicons/apple-touch-icon.png","favicons/browserconfig.xml","favicons/favicon-16x16.png","favicons/favicon-32x32.png","favicons/favicon.ico","favicons/mstile-150x150.png","favicons/safari-pinned-tab.svg","favicons/site.webmanifest"]),
 	mimeTypes: {".mp3":"audio/mpeg",".png":"image/png",".xml":"application/xml",".ico":"image/vnd.microsoft.icon",".svg":"image/svg+xml",".webmanifest":"application/manifest+json"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.a0dc3093.js","app":"_app/immutable/entry/app.79bdf0c0.js","imports":["_app/immutable/entry/start.a0dc3093.js","_app/immutable/chunks/index.d1855a2a.js","_app/immutable/chunks/singletons.a8563c02.js","_app/immutable/entry/app.79bdf0c0.js","_app/immutable/chunks/index.d1855a2a.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.18f08896.js","app":"_app/immutable/entry/app.9ce24497.js","imports":["_app/immutable/entry/start.18f08896.js","_app/immutable/chunks/index.d1855a2a.js","_app/immutable/chunks/singletons.bfa05079.js","_app/immutable/entry/app.9ce24497.js","_app/immutable/chunks/index.d1855a2a.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('./chunks/0-6efa8401.js'),
-			() => import('./chunks/1-00c27f04.js'),
+			() => import('./chunks/1-2f2e787c.js'),
 			() => import('./chunks/2-60616e70.js'),
 			() => import('./chunks/3-3f61258b.js'),
 			() => import('./chunks/4-aa2992ff.js'),
