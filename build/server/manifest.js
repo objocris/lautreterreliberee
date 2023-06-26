90747e46f6653167e2cf4b4a400f0b2b554e158f
Florian Motteau <florian.motteau@nereide.fr>
2023-06-26 19:55:41 +0200

    Update textes

--- build/server/manifest.js
+++ build/server/manifest.js
@@ -4,14 +4,14 @@ const manifest = {
 	assets: new Set(["audio/1.mp3","audio/2.mp3","audio/3.mp3","audio/4.mp3","audio/5.mp3","audio/6.mp3","audio/7.mp3","audio/8.mp3","favicons/android-chrome-192x192.png","favicons/apple-touch-icon.png","favicons/browserconfig.xml","favicons/favicon-16x16.png","favicons/favicon-32x32.png","favicons/favicon.ico","favicons/mstile-150x150.png","favicons/safari-pinned-tab.svg","favicons/site.webmanifest"]),
 	mimeTypes: {".mp3":"audio/mpeg",".png":"image/png",".xml":"application/xml",".ico":"image/vnd.microsoft.icon",".svg":"image/svg+xml",".webmanifest":"application/manifest+json"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.25d52b8d.js","app":"_app/immutable/entry/app.83c64edf.js","imports":["_app/immutable/entry/start.25d52b8d.js","_app/immutable/chunks/index.07a12486.js","_app/immutable/chunks/singletons.1351cfc4.js","_app/immutable/entry/app.83c64edf.js","_app/immutable/chunks/index.07a12486.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.c4f06e64.js","app":"_app/immutable/entry/app.6bbadebd.js","imports":["_app/immutable/entry/start.c4f06e64.js","_app/immutable/chunks/index.07a12486.js","_app/immutable/chunks/singletons.7f6f5763.js","_app/immutable/entry/app.6bbadebd.js","_app/immutable/chunks/index.07a12486.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('./chunks/0-96cecff5.js'),
-			() => import('./chunks/1-28d68a15.js'),
+			() => import('./chunks/1-b1efec70.js'),
 			() => import('./chunks/2-e2a34efc.js'),
 			() => import('./chunks/3-0963eb83.js'),
 			() => import('./chunks/4-03626695.js'),
-			() => import('./chunks/5-0db9daa2.js'),
+			() => import('./chunks/5-8d67726f.js'),
 			() => import('./chunks/6-eeabdd4c.js')
 		],
 		routes: [
